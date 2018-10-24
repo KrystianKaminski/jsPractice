00cbf05a3663b2f8d4be7657c95a6c5f13bc67f8
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-10-24 20:07:45 +0200

    Exponent Number

---
+++ exponentNumber.js
@@ -0,0 +1,9 @@
+const calculateExponent = (a, b) => Math.pow(a, b);
+
+calculateExponent(5, 1);
+calculateExponent(5, 2);
+calculateExponent(5, 3);
+calculateExponent(5, 4);
+calculateExponent(5, 5);
+calculateExponent(5, 0);
+calculateExponent(5, -1);
