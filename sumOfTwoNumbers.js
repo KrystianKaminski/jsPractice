afff16023eec48c9959ee4cff862a5fcf23d14ce
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-10-24 19:55:01 +0200

    Sum of two numbers

---
+++ sumOfTwoNumbers.js
@@ -0,0 +1,7 @@
+const addition = (a, b) => a + b;
+
+console.log(addition(2, 3));
+console.log(addition(2, 5));
+console.log(addition(1, 7));
+console.log(addition(9, 3));
+console.log(addition(11, 33));
