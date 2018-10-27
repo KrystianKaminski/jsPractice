540d99d4f371d613bce855fc2d5c6a895c104b0b
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-10-27 08:30:57 +0200

    stringEndingMatches

---
+++ christmas.js
@@ -0,0 +1,10 @@
+const ifChristmasEve = (date) => {
+    date = new Date();
+    console.log(date);
+
+    // if (date.getDate() === 24 && date.getMonth() === 11) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+}
