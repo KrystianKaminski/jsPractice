91a8bc133e48fa77be4500cf00004cd94bbdf055
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-11-01 13:20:26 +0100

    Primitives vs Objects

--- Functions-Objects/script.js
+++ Functions-Objects/script.js
@@ -1,30 +1,40 @@
-// Object.create
+// Primitives vs objects
 
-const personProto = {
-    calculateAge: function () {
-        console.log(2018 - this.yearOfBirth)
-    }
+
+// Creating a copy
+let a = 23;
+let b = a;
+a = 46;
+console.log(a)
+console.log(b)
+
+
+// No copy was created
+const obj1 = {
+    name: 'John',
+    age: '26'
 }
 
-const john = Object.create(personProto);
-console.log(john);
+const obj2 = obj1
 
-john.name = 'John'
-john.yearOfBirth = 1990
-john.job = 'teacher'
+obj1.age = 30
+console.log(obj1.age)
+console.log(obj2.age)
 
-console.log(john)
+// Functions
 
-const jane = Object.create(personProto, {
-    name: {
-        value: 'Jane'
-    },
-    yearOfBirth: {
-        value: 1969
-    },
-    job: {
-        value: 'designer'
-    }
-})
+let age = 27;
+const obj = {
+    name: 'Krystian',
+    city: 'Lublin'
+}
 
-console.log(jane)
+function change(a, b) {
+    a = 30
+    b.city = 'Warszawa'
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
