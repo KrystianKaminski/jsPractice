69435cf3a1a353e2fe1600b2f95e4af3a1309b3d
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-11-04 13:22:04 +0100

    this part3

--- OOP/script.js
+++ OOP/script.js
@@ -1,46 +1,59 @@
-// this to odwołanie do obiektu
+// Wiązanie domyślne this to obiekt globalny, 'use strict' na to nie pozwala (error/undefined)
 
-// dzięki this nie ma potrzeby uzywania osobnych funkcji dla kazdego obiektu -> mozliwosc wielokrotnego uzycia tej samej funkcji
+// const thisExample = function () {
+//     // 'use strict'
+//     console.log(this.example, this)
+//     const inside = function () {
+//         console.log(this.example, this)
+//     }
+//     inside()
+// }
 
+// thisExample()
 
-function showName() {
-    console.log(this.name)
-}
+// wiązanie niejawne
 
-const carlo = {
-    name: 'Carlo',
-    showName: showName
-}
-
-const angelina = {
-    name: 'Angelina',
-    showName: showName
-}
-
-// carlo.showName()
-// angelina.showName()
-
-// Wiązanie this powstaje w chwili wykonywania metody, nie jest zalezne od miejsca deklaracji, tylko od sposobu wywołania
-
-// W chwili wykonywania funkcji powstaje kontekst
-
-function one() {
-    console.log(this)
-    this.name = 'first'
-    return this.name
-}
-
-const object1 = {
-    two: one,
-    four: function () {
-        console.log(this)
+const user = {
+    age: 45,
+    showName() {
+        console.log(this.age)
+    },
+    showName2: function () {
+        console.log(this.age)
     }
 }
 
-object1.two()
+const stefano = {
+    age: 20,
+    showName: user.showName
+}
 
-const three = object1.two
+// console.log(stefano.showName())
 
-three();
 
-const five = object1.four
+// wiązanie jawne : call i apply
+
+
+const human = {
+    pesel: 12321312312312
+}
+
+const showPesel = function () {
+    console.log(`Twój pesel to: ${this.pesel}`)
+}
+
+// showPesel() // undefined
+// showPesel.call(human)
+
+const showUser = function (name) {
+    console.log(`Uzytkownik ${name} ma ${this.pesel}`)
+}
+
+showUser.call(human, 'Tomasso')
+
+
+// wiązanie twarde - BIND
+
+const showPeselUser = showPesel.bind(human)
+
+showPeselUser()
