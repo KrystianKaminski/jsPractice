2666e7b176a2c9298d01170e4d4a0eb4197ccc27
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-11-04 10:30:43 +0100

    constructor and instances part2

--- OOP/script.js
+++ OOP/script.js
@@ -1,5 +1,13 @@
-const Car = function () {
-
+const Car = function (name) {
+    this.name = name
 }
 
-const maluch = new Car()
+const maluch = new Car('maluch')
+const polonez = new Car('polonez')
+
+// 1. Nowy pusty obiekt
+// 2. Następuje wiązanie this z utworzonym obiektem
+// 3. Powstaje właściwość __proto__
+// 4. Wywołanie funkcji i przypisanie do zmiennej
+
+maluch.name = 'zabytek'
