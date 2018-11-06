d1555888d271308b32cf767e7a78567a7201c132
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-11-06 21:39:40 +0100

    rendering cell and board game

---
+++ minigame/Game.js
@@ -0,0 +1,49 @@
+function Game(selector) {
+    this.container = selector ? document.querySelector(selector) :
+        document.querySelector('body')
+
+    this.gameBoardArray = [
+        [0, 0],
+        [0, 1]
+    ]
+    this.gameBoard = null
+
+    this.init()
+}
+
+Game.prototype.init = function () {
+    this.makeGameBoard()
+    this.render()
+}
+
+Game.prototype.makeGameBoard = function () {
+    const boardElement = document.createElement('div')
+    boardElement.style.width = '500px'
+    boardElement.style.height = '500px'
+    boardElement.style.display = 'flex'
+    boardElement.style.flexWrap = 'wrap'
+
+    this.gameBoard = boardElement
+    this.container.appendChild(boardElement)
+}
+
+Game.prototype.render = function () {
+    this.gameBoardArray.forEach(row => {
+        row.forEach(cell => {
+            this.renderSingleCell(cell)
+        })
+    })
+}
+
+Game.prototype.renderSingleCell = function (cell) {
+    const cellElement = document.createElement('div')
+    cellElement.style.width = '50%'
+    cellElement.style.height = '50%'
+
+    if (cell === 0) {
+        cellElement.style.backgroundColor = 'grey'
+    } else {
+        cellElement.style.backgroundColor = 'black'
+    }
+    this.gameBoard.appendChild(cellElement)
+}
