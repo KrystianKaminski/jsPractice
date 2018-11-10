e7cc2b7801a760b77ffaea95903d3bffa7bb025a
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-11-10 19:22:05 +0100

    Added Wallet class

---
+++ OOP/OOP-Project/Wallet.js
@@ -0,0 +1,29 @@
+class Wallet {
+    constructor(money) {
+        let _money = money
+        // pobieranie aktualnej zawartości portfela
+        this.getWalletValue = () => _money
+
+        // Sprawdzanie czy uzytkownik ma odpowiednią ilość środków         
+        this.checkCanPlay = value => {
+            if (_money >= value) return true
+            return false
+        }
+
+        this.changeWallet = (value, type = "+") => {
+            if (typeof value === 'number' && !isNaN(value)) {
+                if (type === '+') {
+                    return _money += value
+                } else if (type === '-') {
+                    return _money -= value
+                } else {
+                    throw new Error('Nieprawidłowy typ działania')
+                }
+            } else {
+                console.log(typeof value)
+                throw new Error('Nieprawidłowa liczba')
+            }
+        }
+    }
+
+}
