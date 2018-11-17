d685b8d7d59f223b8466e42d98f3fabd6c675f02
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-17 16:12:19 +0100

    Asynchronous behavior

--- ASYNC/script.js
+++ ASYNC/script.js
@@ -1,13 +1,24 @@
-const second = () => {
+function getRecipe() {
     setTimeout(() => {
-        console.log('Async hey there');
-    }, 2000)
+        const recipeID = [523, 883, 432, 974]
+        console.log(recipeID)
+
+        setTimeout((id) => {
+            const recipe = {
+                title: 'Fresh tomato pasta',
+                publisher: 'Krystian'
+            }
+            console.log(`${id}: ${recipe.title}`)
+
+            setTimeout(publisher => {
+                const recipe = {
+                    title: 'Italian pizza',
+                    publisher: 'Krystian'
+                }
+                console.log(recipe)
+            }, 1500, recipe.publisher)
+        }, 1500, recipeID[2])
+    }, 1500)
 }
 
-const first = () => {
-    console.log('Hey there');
-    second()
-    console.log('The end')
-}
-
-first()
+getRecipe()
