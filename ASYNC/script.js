cb134ed1b6da8c2891857e25bc8631c90f9f84d8
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-17 16:24:05 +0100

    Resolve promise

--- ASYNC/script.js
+++ ASYNC/script.js
@@ -1,24 +1,13 @@
-function getRecipe() {
+const getIDs = new Promise((resolve, reject) => {
     setTimeout(() => {
-        const recipeID = [523, 883, 432, 974]
-        console.log(recipeID)
-
-        setTimeout((id) => {
-            const recipe = {
-                title: 'Fresh tomato pasta',
-                publisher: 'Krystian'
-            }
-            console.log(`${id}: ${recipe.title}`)
-
-            setTimeout(publisher => {
-                const recipe = {
-                    title: 'Italian pizza',
-                    publisher: 'Krystian'
-                }
-                console.log(recipe)
-            }, 1500, recipe.publisher)
-        }, 1500, recipeID[2])
+        resolve([523, 883, 432, 974]);
     }, 1500)
-}
+})
 
-getRecipe()
+getIDs
+    .then(IDs => {
+        console.log(IDs)
+    })
+    .catch(error => {
+        console.log(`Error!`)
+    })
