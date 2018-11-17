872657ba2da129c2f22e4ec8ea54840ca076e512
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-17 16:39:30 +0100

    Promises chained example

--- ASYNC/script.js
+++ ASYNC/script.js
@@ -1,12 +1,45 @@
 const getIDs = new Promise((resolve, reject) => {
     setTimeout(() => {
-        reject([523, 883, 432, 974]);
+        resolve([523, 883, 432, 974]);
     }, 1500)
 })
+
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout((ID) => {
+            const recipe = {
+                title: 'Fresh tomato pasta',
+                publisher: 'Krystian'
+            }
+            resolve(`${ID}: ${recipe.title}`)
+
+        }, 1500, recID)
+    })
+}
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => {
+        setTimeout((pub) => {
+            const recipe = {
+                title: 'Italian pizza',
+                publisher: 'Krystian'
+            }
+            resolve(`${pub}: ${recipe.title}`)
+        }, 1500, publisher)
+    })
+}
 
 getIDs
     .then(IDs => {
         console.log(IDs)
+        return getRecipe(IDs[2])
+    })
+    .then(recipe => {
+        console.log(recipe)
+        return getRelated('Krystian')
+    })
+    .then(recipe => {
+        console.log(recipe)
     })
     .catch(error => {
         console.log(`Error!`)
